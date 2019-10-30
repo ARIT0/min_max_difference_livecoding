3ffd22321d939ea37cd80f83f0ba31b7acb8545f
Ariadna Pulido <adna.pulido@gmail.com>
2019-10-30 18:48:45 -0400

    final working version

--- minMax.py
+++ minMax.py
@@ -1,17 +1,32 @@
 def minMax(array):
-    array = input("What is the array?")
     
-    array = [1,2,3,4,5]
     x = 0
-    for x in range(len(array)):
+    z = 1
+
+    for x in range(len(array)-1):
         if array[x] > array[x+1]:
             maxValue = array[x]
-        return maxValue
-    for y in range(len(array)):
-        if array[y] < array[y+1]:
-            minValue = array[y]
-        return minValue
-
+            #if x == len(array)-1:
+                #break
+        #return maxValue
+    
+    for y in range(len(array)-1):
+        if array[y] < array[z]:
+            z = array[y]
+            minValue = array[z]
+            
+        #return minValue
+    
     diff = maxValue - minValue
-    print(diff)
+    return(diff)
+    
 
+def main():
+    array = [1,2,33,4,5,32]
+    # (input("What is the array/list?")) #in order to do this you must loop through 
+                                        # with the number of item that you want to 
+                                        # enter in the list, then one by one add them.
+    print(minMax(array))
+
+if __name__ == "__main__":
+    main()
